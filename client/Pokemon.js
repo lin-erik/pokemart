54e11e2e194be28ba3dcb7d6bd26358a3d4322da
Erik <elin7780@bths.edu>
2018-07-12 15:22:33 -0400

    Basic Pokemon component to hold all Pokemon of user

---
+++ client/Pokemon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Pokemon = (props) => (
+  <div>
+    {props.pokemon.map(poke => {
+      if (poke.pokemonId !== props.currentPokemon.pokemonId) {
+      return (
+      <div key={poke.pokemonId}>
+        <img src={poke.pokeNorm} onClick={ () => { return props.change(poke) }}></img>
+        {poke.pokeName}
+        <div onClick={ () => { return props.delete(poke) }}>Delete</div>
+      </div>
+      )}
+    })}
+  </div>
+)
+
+export default Pokemon;
