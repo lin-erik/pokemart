a0a908989529af451f20e1afe0c6f58c8ef0c6ef
Erik <elin7780@bths.edu>
2018-07-12 15:18:38 -0400

    Express server handles all requests from client

--- server/server.js
+++ server/server.js
@@ -1,5 +1,46 @@
 const express = require('express');
+const models = require('../client/db/models/pokemonModel.js');
+const path = require('path');
+
 const app = express();
+
+app.use(express.static(__dirname + '/../client/dist/'));
+
+app.get('/pokemon', (req, res) => {
+  models.get((err, data) => {
+    if (err) {
+      console.error('Error getting allPokemon', err);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.post('/pokemon', (req, res) => {
+  var body;
+
+  req.on('data', (data) => {body = JSON.parse(data)})
+     .on('end', () => {
+       console.log('POST being processed');
+       
+        models.post(body, (err, data) => {
+          res.send();
+        });
+     });
+});
+
+app.delete('/delete', (req, res) => {
+  var body;
+
+  req.on('data', (data) => {body = JSON.parse(data)})
+     .on('end', () => {
+       console.log('DELETE being processed');
+
+        models.delete(body, (err, data) => {
+          res.send();
+        });
+     });
+});
 
 app.listen(8080, () => {
   console.log('Listening on 8080');
