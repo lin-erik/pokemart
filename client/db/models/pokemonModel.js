85cb3dc24bc9bd7f2644e34b0eb101e57837625d
Erik <elin7780@bths.edu>
2018-07-13 14:45:52 -0400

    New model now adds Pokemon being bought to user group

--- client/db/models/pokemonModel.js
+++ client/db/models/pokemonModel.js
@@ -27,4 +27,37 @@ module.exports = {
       console.log('Deleted from mySQL');
     });
   },
+
+  userAndPokemon: function({pokemonId, userId}, cb) {
+    var command = `INSERT INTO userAndPokemon VALUES (?, ?)`;
+    var params = [userId, pokemonId];
+
+    db.query(command, params, (err, data) => {
+      console.log('Saved to userAndPokemon');
+    });
+  },
+
+  getUserPokemon: function(cb) {
+    var command = `SELECT *
+                   FROM userAndPokemon up
+                   INNER JOIN allPokemon p ON p.pokemonId = up.poke_id
+                   WHERE up.user_id = 1`
+
+    db.query(command, (err, results) => {
+      if (err) {
+        console.error('Error interacting with database', err);
+      } else {
+        cb(null, results);
+      }
+    });
+  }
+
+  // login: function({user, password}, cb) {
+  //   var command = `SELECT * from users WHERE username = ${user} AND password = ${password})`;
+  //   console.log('Processing login');
+
+  //   db.query(command, (err, data) => {
+  //     cb(null, data);
+  //   });
+  // }
 }
