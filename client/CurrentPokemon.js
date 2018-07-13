42b186875f518bd7e68403ee0cd4dd8977eff439
Erik <elin7780@bths.edu>
2018-07-13 14:43:44 -0400

    Shows user's current Pokemon on click

--- client/CurrentPokemon.js
+++ client/CurrentPokemon.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const CurrentPokemon = (props) => {
-  return (
+const CurrentPokemon = (props) => (
     <div>
-      <div>Pokemon of the Day</div>
+      <div>Your Pokemon</div>
       <img src={props.currentPokemon.pokeNorm} height='250' width='250'></img>
       <div>{props.currentPokemon.pokeName}</div>
     </div>
-  )
-}
+)
 
 export default CurrentPokemon;
