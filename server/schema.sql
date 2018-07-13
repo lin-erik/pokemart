429692a87d7d7ddb40a40618c317a291271567fc
Erik <elin7780@bths.edu>
2018-07-13 14:48:33 -0400

    Updated schema, creates users and userAndPokemon tables

--- server/schema.sql
+++ server/schema.sql
@@ -8,3 +8,15 @@ CREATE TABLE allPokemon (
   pokeHeight INT,
   pokeWeight INT
 );
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+  username VARCHAR(255),
+  password VARCHAR(255),
+  money INT DEFAULT 5000
+);
+
+CREATE TABLE userAndPokemon (
+  user_id INT,
+  poke_id INT
+)
