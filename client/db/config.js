977a6a2540830b5fe3a0673a607a73ff592c7184
Erik <elin7780@bths.edu>
2018-07-14 16:12:27 -0400

    Ready for Heroku

--- client/db/config.js
+++ client/db/config.js
@@ -1,10 +1,6 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-  user: 'root',
-  password: 'password',
-  database: 'pokemon'
-});
+const db = mysql.createConnection('mysql://bc64bcf7c741cd:33494f3f@us-cdbr-iron-east-04.cleardb.net/heroku_088aaa0e67b63d4?reconnect=true');
 
 db.connect( (err) => {
   if (err) {
