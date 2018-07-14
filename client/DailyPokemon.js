96f83d98be7bfcaee73d264ef0214f19bd2acdf1
Erik <elin7780@bths.edu>
2018-07-14 15:58:54 -0400

    Prevents constant buying of Pokemon

--- client/DailyPokemon.js
+++ client/DailyPokemon.js
@@ -2,14 +2,25 @@ import React from 'react';
 const script = require('./scripts/index.js');
 
 const DailyPokemon = (props) => {
-  return (
-    <div>
-    <div>Daily Pokemon</div>
-      <img src={props.dailyPokemon.pokeNorm} height='250' width='250' onClick={ () => { return props.handleBuy(props.dailyPokemon) }}></img>
-      <div>{props.dailyPokemon.pokeName}</div>
-      <div>{props.value}</div>
-    </div>
-  )
+    if (props.bought) {
+      return(
+        <div align='center'>
+          <div>Pokemon for Sale</div>
+          <img src={props.dailyPokemon.pokeShiny} height='250' width='250'/>
+          <div>{props.dailyPokemon.pokeName} has been bought</div>
+          <div>E{props.value}</div>
+        </div>
+      )
+    } else {
+        return(
+          <div align='center' className='shadow p-4 mb-4 bg-white'>
+            <div>Pokemon for Sale</div>
+            <img src={props.dailyPokemon.pokeNorm} height='250' width='250' onClick={ () => { return props.handleBuy(props.dailyPokemon) }}/>
+            <div>{props.dailyPokemon.pokeName}</div>
+            <div>E{props.value}</div>
+          </div>
+      )
+    }
 }
 
 export default DailyPokemon;
