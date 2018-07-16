0913b1501e3dd7e4a21b41f2d9eefadbfecc14fd
Erik <elin7780@bths.edu>
2018-07-15 21:25:13 -0400

    Added config.js to .gitignore

--- client/db/config.js
+++
@@ -1,5 +0,0 @@
-const mysql = require('mysql');
-
-const db = mysql.createPool('mysql://bc64bcf7c741cd:33494f3f@us-cdbr-iron-east-04.cleardb.net/heroku_088aaa0e67b63d4?reconnect=true');
-
-module.exports = db;
